1c32c4796fdec05d059e29017ae60ca7a128c559
cychiang <cychiang0823@gmail.com>
2018-07-22 11:53:08 +0800

    dartfmt code & add works in Dart 2 only in pubspec.yaml

--- bin/server.dart
+++ bin/server.dart
@@ -1,5 +1,5 @@
 import 'package:exchanger_server/server_export.dart';
 
 main(List<String> args) async {
-  await new Server().main(args).catchError((onError) => print(onError));
+  await new Server().main(args);
 }
